76fc7cf938f674a17bfa7f37b072c105ac08f937
dichonw <58157142+dichonw@users.noreply.github.com>
2020-05-04 01:35:08 +0700

    membuat main activity

---
+++ app/src/main/java/com/example/biodataapp/Splashscreen.kt
@@ -0,0 +1,12 @@
+package com.example.biodataapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splashscreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
